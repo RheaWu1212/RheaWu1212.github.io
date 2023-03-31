43b3ded17e953fae0862cde9f7757deb17610781
Rhea Wu <rheawu1212@gmail.com>
2023-03-31 17:43:08 -0400

    changes for portfolio file regarding design

---
+++ src/Portfolio.js
@@ -0,0 +1,41 @@
+import './Portfolio.css';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import './assets';
+import HeaderBar from './components/HeaderBar';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+export const Portfolio = () => {
+  return (
+    
+      {/* <HeaderBar />
+      <AboutMe />
+      <Projects />
+      <Contact /> */}
+    <div className='portfolio-p-wrapper'>
+      <div className='portfolio-p'>
+        <div className='portfolio-overlap'>
+          <div className='portfolio-frame' />
+          <h1 className='portfolio-name'>RHEA WU</h1>
+        </div>
+        <div className='portfolio-overlap-group'>
+          <img className='portfolio-stack-gh' src='tabler-brand-github.svg' />
+          <div className='portfolio-overlap-group12'>
+            <div className='portfolio-overlap-group11'>
+              <div className='portfolio-overlap-group10'>
+                <img className='portfolio-stack-git' src='mdi-git.svg' />
+                <img className='portfolio-stack-docker' src='bxl-docker.svg' />
+              </div>
+              <div className='portfolio-overlap-group7'>
+                
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import Portfolio from './Portfolio';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <App />
+    <Portfolio />
   </BrowserRouter>
 );
