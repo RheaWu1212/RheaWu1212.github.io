d59e479349c863f9e4705aca431c09b394991e04
Rhea Wu <rheawu1212@gmail.com>
2023-04-04 18:51:07 -0400

    finish all draft, need to work on animation

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import circle from '../image/circle.svg';
 
 const AboutMe = () => {
     return (
         <div className='aboutme'>
-            <div className='aboutme-wrap'>
             <div className='aboutMe-1'>
                 <p className='aboutme-sum'>
                     but also a human that turns interests into knowledge
@@ -14,28 +14,29 @@ const AboutMe = () => {
                     turns fear into power
                 </p>
             </div>
-            <div className='aboutMe-2'>
-                <a href='https://www.linkedin.com/posts/rheawu-tech_i-was-recently-invited-to-speak-on-load-balancers-activity-7018955111016103936-8k89/?utm_source=share&utm_medium=member_desktop' target='_blank'>
-                    <p className='aboutme-speech'>I've conquered mu fear of public speaking</p>
-                </a>
-                <div className='bg-circle-1' />
-            </div>
+            <div className='aboutme-wrap'>
+                <div className='aboutMe-2'>
+                    <a href='https://www.linkedin.com/posts/rheawu-tech_i-was-recently-invited-to-speak-on-load-balancers-activity-7018955111016103936-8k89/?utm_source=share&utm_medium=member_desktop' target='_blank'>
+                        <p className='aboutme-speech'>I've conquered my fear of public speaking</p>
+                    </a>
+                    <img className='circle' src={circle} />                   
+                </div>
         
-            <div className='aboutMe-3'>
-                <p className='aboutme-mt'>
-                    I'm a Muay Thai practitioner, <br />
-                    classical piano performer
-                </p>
-                <div className='bg-circle-2' />
-            </div>
-            <div className='aboutMe-4'>
-                <p className='aboutme-cat'>
-                    a cat mom, foodie, <br />
-                    vintage collector,
-                </p>
-                <div className='aboutme-more'>more to explore</div>
-                <div className='bg-circle-3' />
-            </div>
+                <div className='aboutMe-3'>
+                    <p className='aboutme-mt'>
+                        I'm a Muay Thai practitioner, <br />
+                        classical piano performer
+                    </p>
+                    <img className='circle' src={circle} />
+                </div>
+                <div className='aboutMe-4'>
+                    <p className='aboutme-cat'>
+                        a cat mom, foodie, <br />
+                        vintage collector,
+                    </p>
+                    <div className='aboutme-more'>more to explore</div>
+                    <img className='circle' src={circle} />
+                </div>
             </div>
         </div>
     )
